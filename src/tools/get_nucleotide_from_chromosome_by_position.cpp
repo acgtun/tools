bee7a74a0f697a6615225b73fcf2821858912af0
chenhaifeng9 <haifengc@usc.edu>
2014-12-17 13:40:01 -0800

    add option for get nucleotide position

--- src/tools/get_nucleotide_from_chromosome_by_position.cpp
+++ src/tools/get_nucleotide_from_chromosome_by_position.cpp
@@ -8,10 +8,19 @@
 #include <iostream>
 #include <algorithm>
 
+#include "option.hpp"
+
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-  ifstream fin(argv[1]);
+  InitProgram(argc, argv);
+
+  string chrom_file;
+  uint32_t chrom_pos;
+  Option::GetOption("-c", chrom_file);
+  Option::GetOption("-p", chrom_pos, 0);
+
+  ifstream fin(chrom_file.c_str());
   string line;
   string chrom_seq;
   while (getline(fin, line)) {
@@ -21,12 +30,9 @@ int main(int argc, const char *argv[]) {
       chrom_seq += line;
     }
   }
-  uint64_t start;
-  istringstream iss(argv[2]);
-  iss >> start;
-  cout << "size of chromosome " << argv[1] << " is " << chrom_seq.size() << endl;
-  cout << "start position: " << start << endl;
-  for (uint64_t i = start, j = 0; i < chrom_seq.size() && j < 100; ++i, ++j) {
+  cout << "size of chromosome " << chrom_file << " is " << chrom_seq.size() << endl;
+  cout << "start position: " << chrom_pos << endl;
+  for (uint64_t i = chrom_pos, j = 0; i < chrom_seq.size() && j < 100; ++i, ++j) {
     cout << chrom_seq[i];
   }
   cout << endl;
