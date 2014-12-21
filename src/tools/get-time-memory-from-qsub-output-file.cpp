d429d0e3ed85307b28d5753ce0572063ec2c51a7
chenhaifeng9 <haifengc@usc.edu>
2014-12-21 10:51:57 -0800

    update get-time-and-memory

--- src/tools/get-time-memory-from-qsub-output-file.cpp
+++ src/tools/get-time-memory-from-qsub-output-file.cpp
@@ -9,27 +9,36 @@
 #include <algorithm>
 
 #include "option.hpp"
+#include "smithlab_os.hpp"
 
 using namespace std;
 
 int main(int argc, const char *argv[]) {
   InitProgram(argc, argv);
-  int num_of_files;
-  Option::GetOption("-n", num_of_files, 1);
+  string files;
+  Option::GetOption("-f", files);
 
-  vector<string> files(num_of_files);
-  for (int i = 0; i < num_of_files; ++i) {
-    char file[4];
-    sprintf(file, "-f%d", i + 1);
-    Option::GetOption(file, files[i]);
+  vector<string> file_names;
+  if(isdir(files.c_str())) {
+    read_dir(files, file_names);
+  } else {
+    file_names.push_back(files);
   }
 
-  for (int i = 0; i < num_of_files; ++i) {
-    ifstream fin(files[i].c_str());
+  for (uint32_t i = 0; i < file_names.size(); ++i) {
+    ifstream fin(file_names[i].c_str());
     string line;
+    bool is_pbs_output_file = false;
     while (getline(fin, line)) {
+      if(line.find("Begin PBS Prologue") != string::npos) {
+        is_pbs_output_file = true;
+        continue;
+      }
       if (line.find("Resources:") == string::npos)
         continue;
+
+      if(is_pbs_output_file == false) 
+        break;
       uint32_t pos = line.find("cput");
       line = line.substr(pos);
       int hour, min, sec;
@@ -38,13 +47,13 @@ int main(int argc, const char *argv[]) {
              &mem);
       uint32_t num_of_seconds = 0;
       num_of_seconds = hour * 3600 + min * 60 + sec;
-      uint32_t p = files[i].find_last_of('/');
+      uint32_t p = file_names[i].find_last_of('/');
       if(p != string::npos) {
-        files[i] = files[i].substr(p + 1);
+        file_names[i] = file_names[i].substr(p + 1);
       }
       cout << (double) num_of_seconds / 3600.00 << "\t" 
           << (double) mem / (1024.00 * 1024.00) << "\t"
-          <<  files[i] << endl; 
+          <<  file_names[i] << endl; 
     }
   }
   return 0;
